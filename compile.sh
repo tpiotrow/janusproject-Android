739f6fcf12eebc810d810168b380c7204a35a633
Vincent Chabrette <vincent.chabrette@gmail.com>
2014-12-03 17:51:42 +0100

    maj compile sh source

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 mvn clean -Dmaven.test.skip=true install
 cp ~/.m2/repository/io/janusproject/io.janusproject.kernel/2.0.2.0-SNAPSHOT/io.janusproject.kernel-2.0.2.0-SNAPSHOT-with-dependencies.jar ../JanusAndroid/app/libs/JanusKernel.jar
+cp ~/.m2/repository/io/janusproject/io.janusproject.kernel/2.0.2.0-SNAPSHOT/io.janusproject.kernel-2.0.2.0-SNAPSHOT-sources.jar ../JanusAndroid/app/libs/JanusKernel-sources.jar
 echo "JanusKernel inserted in JanusAndroid libs."
